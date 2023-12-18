9765b3208b9746a7efb268b5cbdbc954c6b12700
farhanari <farhanarinw@gmail.com>
2023-12-18 21:55:10 +0700

    update 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    container: {
-      center: true,
-      padding: "16px",
-    },
-    extend: {
-      screens: {
-        "2xl": "1320px",
-      },
-    },
+  theme: {},
+  extend: {
+    screens: {},
   },
   plugins: [],
 };
