a35f87a1e9a8bf40ab78a109008244e1eef89abf
farhanari <farhanarinw@gmail.com>
2024-08-10 00:41:00 +0700

    Update Portofolio Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {},
-  extend: {
-    screens: {},
-  },
-  plugins: [],
+    darkMode: "selector",
+    content: ["./src/**/*.{html,js}"],
+    theme: {},
+    extend: {
+        fontFamily: {
+            poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        },
+        screens: {},
+    },
+    plugins: [],
 };
